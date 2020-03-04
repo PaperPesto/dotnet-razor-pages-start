5b1e98eb7a669fc05a9e8befc425b91934fcc06a
Leonardo Bianchini <l.bianchini@aeffegroup.biz>
2020-03-04 12:19:25 +0100

    Namepsace Models

--- Data/RazorPagesMovieContext.cs
+++ Data/RazorPagesMovieContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using RazorPagesMovie;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie.Data
 {
@@ -14,6 +9,6 @@ namespace RazorPagesMovie.Data
         {
         }
 
-        public DbSet<RazorPagesMovie.Movie> Movie { get; set; }
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RazorPagesMovie
+namespace RazorPagesMovie.Models
 {
     public class Movie
     {
